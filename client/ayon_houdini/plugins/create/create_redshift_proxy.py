922967a335b75ad0ceaef7d31543b668e530b0ba
Zheng Jie <zhengjie.zjie@gmail.com>
2024-10-01 11:02:12 +0800

    0.3.14

--- client/ayon_houdini/plugins/create/create_redshift_proxy.py
+++ client/ayon_houdini/plugins/create/create_redshift_proxy.py
@@ -32,18 +32,10 @@ class CreateRedshiftProxy(plugin.HoudiniCreator):
             pre_create_data)
 
         instance_node = hou.node(instance.get("instance_node"))
-        
-        # 1 start MNM
-        export_dir = hou.text.expandString("$HIP/pyblish/")
-        rs_filepath = f"{export_dir}{product_name}/{product_name}.$F4.rs"
-        parms = {
-            "RS_archive_file": rs_filepath,
-        }
 
-        # parms = {
-        #     "RS_archive_file": '$HIP/pyblish/{}.$F4.rs'.format(product_name),
-        # }
-        #1 end MNM
+        parms = {
+            "RS_archive_file": '$HIP/pyblish/{}.$F4.rs'.format(product_name),
+        }
 
         if self.selected_nodes:
             parms["RS_archive_sopPath"] = self.selected_nodes[0].path()
